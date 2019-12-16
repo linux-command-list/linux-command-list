96a8cec7f3d37da3b51c539e30568d6c4c3fc274
Marvin Wendt <git@marvinjwendt.com>
2019-12-16 04:41:26 +0100

    Upload app

---
+++ javascript/app.js
@@ -0,0 +1,75 @@
+getJSON("/commands.json", (err, cmds) => {
+
+  let app = new Vue({
+    el: '#app',
+    data: {
+      search: '',
+      commands: cmds
+    },
+    computed: {
+      filteredList() {
+
+        function compare(a, b) {
+          if (a.cmd < b.cmd)
+            return -1;
+          if (a.cmd > b.cmd)
+            return 1;
+          return 0;
+        }
+
+        return this.commands.filter(cmd => {
+
+          return searchIs(this.search, cmd.cmd)
+          || searchIncludes(this.search, cmd.cmd) 
+          || searchIncludes(this.search, cmd.description)  
+          || searchIncludes(this.search, cmd.example) 
+          || keywordsIncludes(cmd.keywords, this.search)
+
+        }).sort(compare)
+      }
+    }
+  })
+
+})
+
+function searchIs(search, text) {
+  if(text.toLowerCase().includes(search.toLowerCase)) return true
+}
+
+function searchIncludes(search, text) {
+  ret = false
+  text = text.split(" ")
+  search = search.split(" ")
+  text.forEach(txt => {
+    search.forEach(s => {
+      if(txt.toLowerCase().includes(s.toLowerCase())) ret = true
+    })
+  })
+  return ret
+}
+
+function keywordsIncludes(keywords, text) {
+  ret = false
+  text = text.split(" ")
+  text.forEach(txt => {
+    keywords.forEach(keyword => {
+      if(keyword.toLowerCase().includes(txt.toLowerCase())) ret = true
+    })
+  })
+  return ret
+}
+
+function getJSON(url, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.responseType = 'json';
+  xhr.onload = function () {
+    var status = xhr.status;
+    if (status === 200) {
+      callback(null, xhr.response);
+    } else {
+      callback(status, xhr.response);
+    }
+  };
+  xhr.send();
+};
